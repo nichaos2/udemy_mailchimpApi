345fac3175f2cca5021df1907d4b78325d5c9898
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-05-05 17:56:24 +0200

    add comments and clean

--- app.js
+++ app.js
@@ -14,18 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // get root url
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
-  //res.send("This is my project")
+  //res.send("This is my project") // this just sends a line to the browser to check everythin works well
 });
 
+// post root url -> when we push the submit button
 app.post("/", function (req, res) {
   // data I get from the signup page
   //req is the request from our page
   const fname = req.body.fname;
   const lname = req.body.lname;
   const email = req.body.email;
-  console.log(fname);
-  console.log(lname);
-  console.log(email);
 
   // 1. create the data we took from our page
   // create the js object ; this will be the body parameters to send
@@ -48,31 +46,35 @@ app.post("/", function (req, res) {
   const jsonData_from_signup = JSON.stringify(data_from_signup);
 
   // 2. create the request to mailchimp
+  
   // 2.1 url -> see API for mailchimp endpoint : "https://usX.api.mailchimp.com/3.0/lists/{list_id}"
   // and list id at the bottom
-  const X = "8";
+  const X = "8"; // server of the mailchimp; is the last digit in the api key
   const list_id = "3d65e130a6";
   const url = "https://us" + X + ".api.mailchimp.com/3.0/lists/" + list_id;
-  // 2.2 options -> method etc
+  
+  // 2.2 options -> method, authentication, more
   const apiKey = "741486a230d400280bce9be28976c361-us8";
   const options = {
     method: "POST",
     auth: "username_can_be_whatever:" + apiKey,
   };
+
   // 2.3 actual request
   const request_to_mailchimp = https.request(url, options, function (response) {
+    // do smth when getting the response callback function (optional)
     response.on("data", function (data) {
       console.log(JSON.parse(data));
-    }); // end of response.on
-  }); // end of request to mailchimp
+    }); //- end of response.on
+  }); //- end of request to mailchimp
 
   // 3. send the request
   request_to_mailchimp.write(jsonData_from_signup);
 
-  //4. end the request
+  //4. end the request -> this is necessary do NOT forget.
   request_to_mailchimp.end();
 
-}); // end of post(/)
+}); // end of post("/"")
 
 app.listen(3000, function () {
   console.log("App is listening at port 3000");
