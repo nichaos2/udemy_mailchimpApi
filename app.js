c6a76751e6d3a3ca297e599833e06d16f4d9d4a8
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-05-05 11:15:56 +0200

    add bodyparser, and post for the /

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ const request = require("request"); // this is depreceated as of FEB 2020
 const app = express();
 
 // allow to serve static files with express
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+// allow bodyparser read with req.body.<>
+app.use(bodyParser.urlencoded({extended:true}))
 
 // get root url
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/signup.html")
-    //res.send("This is my project")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+  //res.send("This is my project")
 });
 
+app.post("/", function (req, res) {
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var email = req.body.email;
+  console.log(fname);
+  console.log(lname);
+  console.log(email);
+});
 
 app.listen(3000, function () {
   console.log("App is listening at port 3000");
