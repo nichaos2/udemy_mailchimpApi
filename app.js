958f5235876ab92115ea799a396d539596c1f553
Nikolaos IOANNOU <nikolaos.ioannou.e@thalesdigital.io>
2020-05-05 16:03:37 +0200

    add json object to send to mailchimp

--- app.js
+++ app.js
@@ -17,14 +17,40 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var fname = req.body.fname;
-  var lname = req.body.lname;
-  var email = req.body.email;
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const email = req.body.email;
   console.log(fname);
   console.log(lname);
   console.log(email);
+
+// create the js object ; this will be the body parameters to send
+//-> see the API documentation 
+    const data = {
+        members : [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: fname,
+                    LNAME: lname
+                }
+            }
+        ]
+    };
+// create the JSON object string - flatpack JSON 
+// -> this is what we send 
+    const jsonData = JSON.stringify(data);
+
 });
 
 app.listen(3000, function () {
   console.log("App is listening at port 3000");
 });
+
+
+// API Key - Mailchimp
+// 741486a230d400280bce9be28976c361-us8
+
+// List ID - Mailchimp
+// 3d65e130a6
